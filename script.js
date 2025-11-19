ba7f1aa2bdb40a6c70eeaaf4193a1441c64f2a70
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2025-11-19 13:43:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
+let list = document.getElementById("infi-list")
+//let fragment = document.createDocumentfragment()
+let c = 0;
 
+for(let i=0; i<=10; i++){
+	let item = document.createElement("li")
+	item.textContent = "Item " + c
+	list.appendChild(item)
+	//fragment.appendChild(item)
+	c+=1
+}
+//list.appendChild(fragment)
+
+list.addEventListener("scroll", () => {
+	if(list.scrollTop + list.clientHeight >= list.scrollHeight-1){
+	    let item = document.createElement("li")
+	    item.textContent = "Item " + c;
+	    list.appendChild(item)
+		c++
+	}
+})
